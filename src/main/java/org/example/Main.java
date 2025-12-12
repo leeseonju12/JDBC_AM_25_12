034ade77e361d01d52612eb918030a24302ad06f
leeseonju12 <seonju220@gmail.com>
2025-12-12 14:50:44 +0900

    게시글 작성 시 DB에 insert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -28,6 +32,52 @@ public class Main {
                 String title = sc.nextLine().trim();
                 System.out.print("내용 : ");
                 String body = sc.nextLine().trim();
+
+                //DB 연동
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "INSERT INTO article";
+                    sql += " SET regDate = NOW(),";
+                    sql += "updateDate = NOW(),";
+                    sql += "title =  title";
+                    sql += "`body` =  body";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+                    pstmt.setString(1, title);
+                    pstmt.setString(2, body);
+
+                    int affectedRow =  pstmt.executeUpdate();
+                    System.out.println("affectedRow = " + affectedRow);
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
 
                 Article article = new Article(id, title, body);
                 articles.add(article);
